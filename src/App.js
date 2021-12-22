6e977383a1122f453fcddb985e0fb2328dff1b6a
nana <nykim@nykim.net>
2021-12-22 18:02:04 +0900

    practice `useEffect()`

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState('');
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+
+  const iRunOnlyOnce = () => console.log('I run only once!');
+  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    if (keyword !== '') {
+      console.log(`keyword' changes: SEARCH FOR ${keyword}`);
+    }
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Movie</h1>
-      <Button text="Hey!"></Button>
+      <input
+        type="text"
+        placeholder="Search here..."
+        value={keyword}
+        onChange={onChange}
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
