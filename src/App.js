ef3e8a25fbacfe4e50172662a35d69183dbd298a
nana <nykim@nykim.net>
2021-12-24 16:39:29 +0900

    show To Do List (part 2)

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ function App() {
       return;
     }
     setTodos((currentTodos) => [toDo, ...currentTodos]);
+    // setTodos(function (currentTodos) { return [toDo, ...currentTodos]; });
+    // argument로 현재 state를 넘겨준다.
     setToDo('');
-    // 왜 이벤트 내부에서 콘솔을 찍으면 이전 state까지만 표시되는지 궁금
   };
   console.log(toDos);
+  // 왜 이벤트 내부에서 콘솔을 찍으면 이전 state까지만 표시되는지 궁금
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -26,6 +29,13 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((todo, idx) => (
+          <li key={idx}>{todo}</li>
+        ))}
+      </ul>
+      {/* map() 메서드는 새로운 배열을 리턴한다! */}
     </div>
   );
 }
