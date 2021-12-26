a91d0b05c0e2b6e6c7c8f022075d2fe1751cbe39
nana <nykim@nykim.net>
2021-12-26 17:29:16 +0900

    use react-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/hello" element={<p>Hello ;)</p>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 function Movie({ title, cover, summary, genres }) {
   return (
     <div>
       <div>
-        <h2>{title}</h2>
+        <h2>
+          <Link to="/movie">{title}</Link>
+          {/* <a href="/movie">{title}</a> */}
+        </h2>
         <img src={cover} alt={title} />
         <p>{summary}</p>
         <ul>
