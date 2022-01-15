30ab51277e4b15d192156a0be62e8d3193d60c8f
nana <nykim@nykim.net>
2022-01-15 14:38:31 +0900

    deploy project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Detail from './routes/Detail';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={`/`} element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/hello" element={<p>Hello ;)</p>} />
       </Routes>
